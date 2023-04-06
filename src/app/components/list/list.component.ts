48e9361a5253f92f7df7aaa41454a9d88d4de961
shuabrannigan <joshua.wddg@gmail.com>
2023-04-06 11:17:26 +0930

    added paginator to table

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IListComponent } from './list.types';
+import { IListComponent, mockEventList } from './list.types';
 import { Observable, of } from 'rxjs';
 import { TicketMasterEventList } from 'src/app/misc/event.types';
 
@@ -9,7 +9,7 @@ import { TicketMasterEventList } from 'src/app/misc/event.types';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements IListComponent {
-  list$: Observable<TicketMasterEventList> = of({events: [],page: null})
+  list$: Observable<TicketMasterEventList> = of(mockEventList)
   loading$: Observable<boolean> = of(false)
 
   displayedColumns = ['name', 'venueName', 'startDate', 'endDate']
