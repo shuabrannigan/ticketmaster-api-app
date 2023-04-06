330bdfeb28a6eaf1e4b336b68fee45256409eec8
shuabrannigan <joshua.wddg@gmail.com>
2023-04-06 09:47:57 +0930

    added matbuttonmodule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
 const MaterialModules = [
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
 ]
 
 @NgModule({
