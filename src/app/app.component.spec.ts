93f972713bb0f9b5853b0b601e6c2e33b0056e90
shuabrannigan <joshua.wddg@gmail.com>
2023-04-06 09:35:59 +0930

    added toolbar to app.component html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ToolbarComponent} from './components/toolbar/toolbar.component'
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let appComponent: AppComponent
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, ToolbarComponent
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+
+    expect(appComponent).toBeTruthy();
   });
+
+  it('should render app-toolbar', () => {
+    const toolbar = fixture.nativeElement.querySelector('app-toolbar')
+    expect(toolbar).toBeTruthy()
+  })
 });
