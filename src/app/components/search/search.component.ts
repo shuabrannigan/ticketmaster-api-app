2b069c060f33680317b554a68e0ee40bc62b98e2
shuabrannigan <joshua.wddg@gmail.com>
2023-04-06 14:59:04 +0930

    wired up search and clear

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ISearchComponent } from './search.types';
 import { FormControl, FormGroup } from '@angular/forms';
+import { TicketMasterQueryService } from 'src/app/shared/services/other/ticketmaster-query.service';
 
 @Component({
   selector: 'app-search',
@@ -8,13 +9,20 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements ISearchComponent {
+
+  constructor(private ticketMasterQueryService: TicketMasterQueryService) {}
+
   searchForm: FormGroup<any> = new FormGroup({
     city: new FormControl(''),
     startDateTime: new FormControl(''),
     endDateTime: new FormControl('')
   })
   search(): void {
-    throw new Error('Method not implemented.');
+    this.ticketMasterQueryService.getEvents(this.searchForm.value)
+  }
+
+  clear(): void {
+    this.searchForm.reset({city: '', startDateTime: '', endDateTime: ''})
   }
 
 }
