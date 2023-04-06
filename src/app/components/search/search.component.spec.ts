53c0e83514e15cd4b6baa3b13e8d34608ab64ee0
shuabrannigan <joshua.wddg@gmail.com>
2023-04-06 10:23:27 +0930

    added test for search elements

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
 import { TicketMasterApiSearchTerm } from './search.types';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -9,6 +10,7 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule],
       declarations: [ SearchComponent ]
     })
     .compileComponents();
@@ -30,5 +32,48 @@ describe('SearchComponent', () => {
     }
     expect(component.searchForm.value).toEqual(expectedFormValues)
   })
+
+  describe('Search Button', () => {
+    it('should render button', () => {
+      const button = fixture.nativeElement.querySelector('[data-test="searchButton"]')
+      expect(button instanceof HTMLButtonElement).toBe(true)
+      expect(button.innerText).toEqual('Search')
+    })
+
+    it('click event should call search() method', () => {
+      const searchSpy = spyOn(component, 'search')
+      const button = fixture.nativeElement.querySelector('[data-test="searchButton"]')
+      button.click()
+      expect(searchSpy).toHaveBeenCalled()
+    })
+  })
+
+  describe('City Input', () => {
+    it('should render input', () => {
+      const input = fixture.nativeElement.querySelector('[data-test="cityInput"]')
+      expect(input instanceof HTMLInputElement).toBe(true)
+    })
+
+    it('when formControl city updates, value of input should update', () => {
+      const expectedValue = 'Adelaide'
+      component.searchForm.patchValue({city: expectedValue})
+      const input = fixture.nativeElement.querySelector('[data-test="cityInput"]')
+      expect(input.value).toEqual(expectedValue)   
+    })
+  })
+
+  describe('StartDateTime Input', () => {
+    it('should render input', () => {
+      const input = fixture.nativeElement.querySelector('[data-test="startDateInput"]')
+      expect(input instanceof HTMLInputElement).toBe(true)
+    })
+
+    it('when formControl startDateTime updates, value of input should update', () => {
+      const date = new Date('2023-04-04').toISOString()
+      component.searchForm.patchValue({startDateTime: date})
+      
+    })
+  })
+
 
 });
